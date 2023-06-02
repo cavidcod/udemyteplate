535a53a8ac929d5968512fe792e1c3833bebb7d4
Idrisov Cavid <aztucavidaztu@gmail.com>
2023-06-02 22:05:21 +0400

    first commit

---
+++ assets/script/script.js
@@ -0,0 +1,10 @@
+let slide1 = document.querySelector(".course-cards");
+let i = 0;
+
+
+function slide(){
+slide1.style.transform = "translateX(-750px)"
+}
+function slide2(){
+    slide1.style.transform = "translateX(750px)"
+}
